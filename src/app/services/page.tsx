99f5b6937e00f4da6d0c02404e3dbac6326d07eb
JacekEBA <jacek@ebautomations.com>
2025-08-23 03:41:29 -0400

    Create page.tsx

---
+++ src/app/services/page.tsx
@@ -0,0 +1,3 @@
+import ServicesPage from "../../components/pages/ServicesPage";
+export const metadata = { title: "Services • RCS Service" };
+export default function Page() { return <ServicesPage />; }
