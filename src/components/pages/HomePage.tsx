7e65298e774682943f9e334235ac4901a70244e4
JacekEBA <jacek@ebautomations.com>
2025-08-23 02:12:20 -0400

    Update HomePage.tsx

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,13 +1,15 @@
 export default function HomePage() {
   return (
-    <h2 className="text-xl text-primary bg-secondary p-4 rounded">
-  Styled Test Box 🚀
-</h2>
+    <>
+      <h2 className="text-xl text-primary bg-secondary p-4 rounded">
+        Styled Test Box 💅
+      </h2>
 
-    <main className="bg-background text-foreground min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl border border-border p-6 rounded">
-        ✅ Tailwind + CSS Variables are Working
-      </h1>
-    </main>
+      <main className="bg-background text-foreground min-h-screen flex items-center justify-center">
+        <h1 className="text-2xl border border-border p-6 rounded">
+          ✅ Tailwind + CSS Variables are Working
+        </h1>
+      </main>
+    </>
   );
 }
