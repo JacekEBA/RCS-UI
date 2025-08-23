280cc4243de09ee4988af482fdaac76777baa663
JacekEBA <jacek@ebautomations.com>
2025-08-23 04:09:20 -0400

    Create page.tsx

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,4 @@
+import ContactPage from "../../components/pages/ContactPage";
+
+export const metadata = { title: "Contact • RCS Service" };
+export default function Page() { return <ContactPage />; }
