8c84809433e7f1fcac62e430a82ff7c42fbd28b4
JacekEBA <jacek@ebautomations.com>
2025-08-23 01:14:27 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}", // ✅ includes all .tsx and .ts files in `src/`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
