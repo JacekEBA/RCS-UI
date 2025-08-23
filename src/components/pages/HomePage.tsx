56e1bc897a9774ed043ec89b0a1fc1ca90e1c987
JacekEBA <jacek@ebautomations.com>
2025-08-23 02:10:40 -0400

    Update HomePage.tsx

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,5 +1,9 @@
 export default function HomePage() {
   return (
+    <h2 className="text-xl text-primary bg-secondary p-4 rounded">
+  Styled Test Box 🚀
+</h2>
+
     <main className="bg-background text-foreground min-h-screen flex items-center justify-center">
       <h1 className="text-2xl border border-border p-6 rounded">
         ✅ Tailwind + CSS Variables are Working
