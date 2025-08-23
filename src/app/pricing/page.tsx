7826843df421813f21b71a3e4ec518351ab6744d
JacekEBA <jacek@ebautomations.com>
2025-08-23 03:42:05 -0400

    Create page.tsx

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,3 @@
+import PricingPage from "../../components/pages/PricingPage";
+export const metadata = { title: "Pricing • RCS Service" };
+export default function Page() { return <PricingPage />; }
