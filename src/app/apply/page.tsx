194150d86b34ea228e971d2b33dbc77c84cad6ee
JacekEBA <jacek@ebautomations.com>
2025-08-23 04:02:35 -0400

    Create page.tsx

---
+++ src/app/apply/page.tsx
@@ -0,0 +1,7 @@
+import ApplyPage from "../../components/pages/ApplyPage";
+
+export const metadata = { title: "Apply • RCS Service" };
+
+export default function Page() {
+  return <ApplyPage />;
+}
