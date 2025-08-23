54396bd327b6cf212b177dcbda9accaa4ca8b15a
JacekEBA <jacek@ebautomations.com>
2025-08-22 23:40:00 -0400

    Create RCSFeatureShowcase.tsx

---
+++ src/components/RCSFeatureShowcase.tsx
@@ -0,0 +1,8 @@
+export default function RCSFeatureShowcase() {
+  return (
+    <div>
+      <h2>RCS Feature Showcase</h2>
+      <p>This is a placeholder component for the feature showcase.</p>
+    </div>
+  );
+}
