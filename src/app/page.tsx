6134c912189f47f7e4005e2b79fe0f56e89214a2
JacekEBA <jacek@ebautomations.com>
2025-08-22 23:26:24 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
+import HomePage from "@/components/pages/HomePage";
+
 export default function Page() {
-  return (
-    <main className="min-h-screen bg-background text-foreground flex items-center justify-center">
-      <div className="rounded-xl border border-border p-6">
-        Tailwind + CSS variables are working ✅
-      </div>
-    </main>
-  );
+  return <HomePage />;
 }
